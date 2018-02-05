ef9bc3674aea8a3519ab1b1da4bc6b43b90882de
Pikachu <valeriu.rusu99@gmail.com>
2018-02-05 17:16:09 +0200

    basic add

---
+++ SmackDat/Controller/ChatVC.swift
@@ -0,0 +1,25 @@
+//
+//  ChatVC.swift
+//  SmackDat
+//
+//  Created by Minami on 1/25/18.
+//  Copyright © 2018 Minami. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    // Outlets
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+    }
+
+   
+
+}
